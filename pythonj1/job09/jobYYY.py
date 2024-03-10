3407ad3f2d9797670982ad4f4aa8e2b29fb4aafc
alexandre-imbernon <160838888+alexandre-imbernon@users.noreply.github.com>
2024-03-10 23:01:56 +0100

    Delete pythonj1/job09/jobYYY.py

--- pythonj1/job09/jobYYY.py
+++
@@ -1,8 +0,0 @@
-info = "Nos melons sont bios en provenance de France."
-prix = "Le tarif à l'unité est de 2,50€."
-qté = "Nous avons actuellement 6 melons en stock."
-
-print("info","prix","qté")
-
-
-(f"info {'HN':>4s}")
