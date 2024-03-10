3f4df4660d7b7a1d907dc219efc92ddda80d547c
alexandre-imbernon <160838888+alexandre-imbernon@users.noreply.github.com>
2024-03-10 23:03:58 +0100

    Delete pythonj4/job11/expln.py

--- pythonj4/job11/expln.py
+++
@@ -1,3 +0,0 @@
-i + 1 : Cette ligne est placée avant la boucle for, donc i n’est pas défini à ce moment-là. De plus, le résultat de i + 1 n’est pas stocké ou utilisé, donc cette ligne n’a pas d’effet.
-for i in L: : Cette boucle for parcourt chaque élément i dans L, mais elle ne modifie pas les valeurs dans L.
-return L : Cette instruction est à l’intérieur de la boucle for, donc la fonction se termine et renvoie L après la première itération. Pour renvoyer le résultat final après que toutes les modifications ont été faites, return L doit être placé à l’extérieur de la boucle for.
